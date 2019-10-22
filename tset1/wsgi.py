f8cb74cf68d69967f59e61e699f6f0a94f58b2e8
guangyaw <guangyaw@hotamil.com>
2019-10-22 17:57:29 +0800

    test online

--- tset1/wsgi.py
+++ tset1/wsgi.py
@@ -7,25 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tset1.settings')
-#
-# application = get_wsgi_application()
-
 import os
 import sys
 from os.path import join,dirname,abspath
 
+from django.core.wsgi import get_wsgi_application
+
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
 
 sys.path.insert(0, PROJECT_DIR)
-sys.path.append('/home/xyaw/web_pay_v35/xvenv/Lib/site-packages')
+sys.path.append('/home/xyaw/tset1/xvenv/Lib/site-packages')
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'web_pay_v35.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tset1.settings')
 
 application = get_wsgi_application()
+
+
+
+
+
+
+
+
+
