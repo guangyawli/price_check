12eff6ae3aa76911b442f9a6fab586ac08f495d5
guangyaw <guangyaw@hotamil.com>
2019-10-25 13:44:26 +0800

    online for autoemail

--- tset1/wsgi.py
+++ tset1/wsgi.py
@@ -15,7 +15,7 @@ from os.path import join,dirname,abspath
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
 
 sys.path.insert(0, PROJECT_DIR)
-sys.path.append('/home/xyaw/auto_mail/xvenv/lib/python3.6/site-packages/')
+sys.path.append('/home/xyaw/auto_mail/xenv/lib/python3.5/site-packages/')
 
 
 from django.core.wsgi import get_wsgi_application
